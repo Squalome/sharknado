9113af2c57a68ac0d0b9fc14d6cda64403d32279
Allard <AllardQuek@users.noreply.github.com>
2023-10-07 18:27:39 +0200

    Use Address component and bold question ID

--- packages/nextjs/components/QuestList.jsx
+++ packages/nextjs/components/QuestList.jsx
@@ -8,24 +8,27 @@ export const QuestList = () => {
     <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
       <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
         <Quest
-          question="#06 Who is cooler: Shark or Horse?"
-          contractAddress="0xBC4CA...6f13D"
+          question_id="#06"
+          question="Who is cooler: Shark or Horse?"
+          contractAddress="0xDe00050C5eCBA5E32E1D0b57e1f6669184f4fC15"
           reward="0.1 ETH"
           sharks="6/7"
           optionA="Shark"
           optionB="Horse"
         />
         <Quest
-          question="#07 Pineapple on pizza?"
-          contractAddress="0xBC4CA...6f13D"
+          question_id="#07"
+          question="Pineapple on pizza?"
+          contractAddress="0xDe00050C5eCBA5E32E1D0b57e1f6669184f4fC15"
           reward="1 ETH"
           sharks="1/7"
           optionA="Yes"
           optionB="No"
         />
         <Quest
-          question="#08 ETH or BTC?"
-          contractAddress="0xBC4CA...6f13D"
+          question_id="#08"
+          question="ETH or BTC?"
+          contractAddress="0xDe00050C5eCBA5E32E1D0b57e1f6669184f4fC15"
           reward="0.68 ETH"
           sharks="4/7"
           optionA="ETH"
