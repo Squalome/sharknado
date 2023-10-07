a9e23510be1d43419d45f9103ae86eba1fb6f087
Allard <AllardQuek@users.noreply.github.com>
2023-10-07 11:39:17 +0200

    Add summary to home page

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -18,36 +18,31 @@ const Home: NextPage = () => {
 
         <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
           <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contract
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <SparklesIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Experiment with{" "}
-                <Link href="/example-ui" passHref className="link">
-                  Example UI
-                </Link>{" "}
-                to build your own UI.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+            <div className="flex flex-col bg-base-100 px-10 py-10 text-left items-left max-w-xs rounded-3xl">
               <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
+
+              <p>01 | Join the Quest</p>
+              <p>Got an NFT? Find a bait that tickles your fins and hop on.</p>
             </div>
+            <div className="flex flex-col bg-base-100 px-10 py-10 text-left items-left max-w-xs rounded-3xl">
+              <BugAntIcon className="h-8 w-8 fill-secondary" />
+              <p>02 | Create ZK Proof</p>
+              <p>Authenticate your NFT ownership with ZK magic.</p>
+            </div>
+            <div className="flex flex-col bg-base-100 px-10 py-10 text-left items-left max-w-xs rounded-3xl">
+              <SparklesIcon className="h-8 w-8 fill-secondary" />
+              <p>03 | Vote & Win</p>
+              <p>Cast your vote and cross your fins for the big catch!</p>
+            </div>
+          </div>
+
+          {/* Button that links to quests page */}
+          <div className="flex justify-center items-center mt-12">
+            <Link href="/quests">
+              <button className="btn btn-secondary btn-lg normal-case font-thick bg-base-200">
+                Fin-tastic, let&rsquo;s go!
+              </button>
+            </Link>
           </div>
         </div>
       </div>
