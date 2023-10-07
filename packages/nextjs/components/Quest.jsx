0d0608ab910284ef5f245e2f752a8c959f8f9a83
Allard <AllardQuek@users.noreply.github.com>
2023-10-07 18:27:56 +0200

    Add signing transactions popup

--- packages/nextjs/components/Quest.jsx
+++ packages/nextjs/components/Quest.jsx
@@ -1,17 +1,23 @@
 import React, { useState } from "react";
+import { Address } from "./scaffold-eth/Address";
 
 /**
  * Quest question card
  */
-export const Quest = ({ question, reward, sharks, contractAddress, optionA, optionB }) => {
+export const Quest = ({ question_id, question, reward, sharks, contractAddress, optionA, optionB }) => {
   const [isSelected, setIsSelected] = useState(false);
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(true);
   const [response, setResponse] = useState("");
   const [walletAddress, setWalletAddress] = useState("");
 
   const handleClick = () => {
     setIsSelected(!isSelected);
-    setIsSubmitted(!isSubmitted);
+    setIsModalOpen(true);
+
+    if (!isSubmitted) {
+      setIsSubmitted(!isSubmitted);
+    }
     console.log(`Submitted ${response} with wallet ${walletAddress}`);
   };
 
@@ -26,11 +32,13 @@ export const Quest = ({ question, reward, sharks, contractAddress, optionA, opti
 
       <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
         {/* Quest details  */}
-        <p>Eligible Contract address: {contractAddress}</p>
+        <strong>{question_id}</strong>
+        <Address address={contractAddress} />
         <p>{question}</p>
         <p>Bait: {reward}</p>
         <p>Sharks: {sharks}</p>
 
+        {/* Show button to join quest or show options if quest is already joined  */}
         {isSelected ? (
           // Options
           <div className="flex flex-col bg-base-100 px-12 pt-6 text-center items-center max-w-xs rounded-3xl">
@@ -73,6 +81,34 @@ export const Quest = ({ question, reward, sharks, contractAddress, optionA, opti
           </button>
         )}
       </div>
+
+      {/* Show modal popup if response is submitted */}
+      {isSubmitted ? (
+        <>
+          {/* Check checkbox if modal should be open */}
+          {isModalOpen ? <input type="checkbox" id="my-modal" class="modal-toggle" checked /> : null}
+
+          <dialog id="my-modal" className="modal">
+            <div className="modal-box bg-blue-600 float-left">
+              <progress className="progress progress-warning w-100 mb-6"></progress>
+              <h1 className=" font-bold text-lg">Sign Transactions</h1>
+              <p className=" text-md">Please open your wallet to sign the following transactions:</p>
+              <ul className="steps steps-vertical">
+                <li className="step step-primary">Create Semiphore ID</li>
+                <li className="step step-primary">ZK Proof</li>
+                <li className="step step-primary">Vote in the Quest and join lottery 💦</li>
+              </ul>
+              <div className="modal-action">
+                <form method="dialog">
+                  <button onClick={() => setIsModalOpen(false)} className="btn btn-secondary">
+                    Sign transactions
+                  </button>
+                </form>
+              </div>
+            </div>
+          </dialog>
+        </>
+      ) : null}
     </div>
   );
 };
